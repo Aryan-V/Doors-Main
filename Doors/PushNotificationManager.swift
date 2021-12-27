c0e417cc25e36952a6159828906bc616c30e02af
Aryan-V <45548390+Aryan-V@users.noreply.github.com>
2021-12-27 14:52:46 -0800

    adding firestore

--- Doors/PushNotificationManager.swift
+++ Doors/PushNotificationManager.swift
@@ -13,16 +13,19 @@ import UserNotifications
 
 class PushNotificationManager: NSObject, MessagingDelegate, UNUserNotificationCenterDelegate {
     
+    let defaults = UserDefaults.standard
+    
     //OJU
     //error: with remoteMessaging and next func
-    let userID: String
-    init(userID: String) {
-        self.userID = userID
-        super.init()
-    }
+//    let userID: String
+//    init(userID: String) {
+//        self.userID = userID
+//        super.init()
+//    }
+    
     func updateFirestorePushTokenIfNeeded() {
         if let token = Messaging.messaging().fcmToken {
-            let usersRef = Firestore.firestore().collection("users_table").document(userID)
+            let usersRef = Firestore.firestore().collection("users_table").document(defaults.string(forKey: "userName")!)
             usersRef.setData(["fcmToken": token], merge: true)
         }
     }
